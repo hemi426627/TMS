0d247d842a055f81bbdb635303ca701fef040a81
hemi426627 <hammas426627@gmail.com>
2026-02-13 16:48:27 +0500

    Delete Task_Management_System directory

--- Task_Management_System/Task_Management_System/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask  # type: ignore
-
-app = Flask(__name__)
-
-from Task_Management_System import views
-
-from Task_Management_System.blueprints.api import api
-
-app.register_blueprint(api)
